29c04d944d84c5e2ab647d85f6227e0b1d08f3bb
Nicholas Strzelczyk <17nas6@queensu.ca>
2024-02-13 13:09:30 -0500

    added some todo's based on KHZ feedback

--- TestEnvironmentGUI/src/App.java
+++ TestEnvironmentGUI/src/App.java
@@ -1,5 +1,10 @@
 import javax.swing.SwingUtilities;
 
+// TODO: make the data append to a csv file
+// TODO: measure the time taken for a user to guess the target
+// TODO: make it easier to run tests using various sizes of activation radius'
+// TODO: make a calibration round to center the mouse
+
 public class App {
     public static void main(String[] args){
         SwingUtilities.invokeLater(new Runnable() {
